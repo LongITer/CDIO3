ad3a07e232f7aaaf2de93546c7915b757e7ae61f
Long <longiter04@gmail.com>
2025-04-05 21:16:49 +0700

    Refactored All Project

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
-const { log } = require('console');
 const express = require('express');
-const fs = require('fs');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
-// app.get('/', (req, res) => {
-//     res.status(200).json({message: 'Hello from the server side !!!', app: 'Natours'})
-// });
+app.use(express.static(`${__dirname}/public`));
 
-// app.post('/', (req, res) => {
-//     res.send('You are post tot this endpoint')
-// })
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
 
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        result: tours.length,
-        data: {
-            tours: tours
-        }
-    })
-})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-app.post('/api/v1/tours', (req, res) => {
-    console.log(req.body);
-    res.status(200).json({
-        status: 'success',
-    })   
-})
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
+module.exports = app;
